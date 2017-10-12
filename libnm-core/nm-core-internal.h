8e7719d70ce37fbceb6dac7228bba0548f41742a
Francesco Giudici <fgiudici@redhat.com>
2017-10-12 12:02:06 +0200

    libnm-core: add explicit team properties to NMSettingTeam

--- libnm-core/nm-core-internal.h
+++ libnm-core/nm-core-internal.h
@@ -427,7 +427,11 @@ gboolean _nm_utils_inet6_is_token (const struct in6_addr *in6addr);
 
 /*****************************************************************************/
 
-gboolean    _nm_utils_team_config_equal (const char *conf1, const char *conf2, gboolean port);
+gboolean _nm_utils_team_config_equal (const char *conf1, const char *conf2, gboolean port);
+char *_nm_utils_team_config_get (const char *conf,
+                                 const char *key,
+                                 const char *subkey,
+                                 gboolean port_config);
 
 /*****************************************************************************/
 
